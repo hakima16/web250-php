c810e8adede442b08441f7709b92141b5c530fb2
hakima16 <chhakima87@gmail.com>
2026-03-09 17:09:33 -0400

    Create introduction.php

---
+++ introduction.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Introduction</title>
+  <link rel="stylesheet" type="text/css" href="styles/default.css">
+</head>
+
+<body>
+  <!-- Pulls in header.html -->
+<div data-include="components/header.html"></div>
+
+  <main>
+    <h2>Introduction</h2>
+
+    <!-- Profile Image -->
+  <figure style="text-align: center;">
+    <img src="images/HakimaChabane.jpg" alt="When I was fully happy in 2020" width="400">
+    <figcaption>When I was fully happy in 2020</figcaption>
+  </figure>
+
+    <!-- Personal Paragraph -->
+    <p>Hi everyone! I’m excited to be in this class and looking forward to learning and sharing ideas with you all. I’m happy to share that I’m in my last semester and graduating in May—what an achievement!</p>
+	
+
+    <!-- Personal & Professional Info -->
+    <ul>
+      <li><strong>Personal Background:</strong> I am originally from North Africa and now live in the U.S., balancing being a mom, working, and going to school. I enjoy learning new things, especially in technology, and I’m focused on building a better future for myself and my family.</li>
+      <li><strong>Professional Background:</strong> I currently work as a Banker at Truist Bank. Before that, I worked in fashion retail stores, gaining experience in customer service and sales. I’m gaining both sales and tech skills and working toward transitioning into a tech-related role in the future.</li>
+      <li><strong>Academic Background:</strong> I am pursuing a degree in Full Stack Programming. I have completed courses in Python, software development, and project management. This semester I am focusing on web development and professional communication to strengthen both technical and workplace skills.</li>
+      <li><strong>Background in this Subject:</strong> I don’t have much coding experience but have used software like MATLAB and ANSYS. Those gave me a small introduction to programming, but I’m still new overall. I’m looking forward to building my skills in this class.</li>
+      <li><strong>Primary Computer Platform:</strong> I mainly use a Windows computer for schoolwork and daily tasks, running Windows 11.</li>
+      <li><strong>Courses I’m Taking & Why:</strong>
+        <ol>
+          <li><strong><a href="contract.html">WEB250 - Database Driven Websites;</a></strong> To develop a strong understanding of how websites interact with databases, learning to design, query, and manage data effectively. This will help me build more dynamic web applications for my current work and prepare me for future projects in web development.</li>
+          <li><strong>CSC221;</strong> - Advanced Python Programming; To deepen my Python skills, focusing on advanced programming concepts and problem-solving techniques. This will improve my ability to handle complex tasks at work and strengthen my foundation for future technology roles.</li>
+          <li><strong>WEB215;</strong> - Advanced Markup and Scripting; To build on my existing web development skills by learning more advanced HTML, CSS, and JavaScript techniques. This course will help me create more interactive, well-structured websites and better prepare me for real-world web development projects and future technology roles</li>
+        </ol>
+      </li>
+      <li><strong>Funny/Interesting Item to Remember Me By:</strong> I measure productivity in cups of coffee.</li>
+      <li><strong>Also to Share:</strong> When I’m in the U.S., I miss Algeria; when I’m in Algeria, I miss the U.S. Sometimes it feels like I don’t fully exist in either place.</li>
+    </ul>
+
+    <!-- Quote -->
+   <!-- Quote at the end of main -->
+    <blockquote style="text-align: center;">
+    "Education is not preparation for life; education is life itself."<br>
+     — John Dewey
+    </blockquote>
+
+  </main>
+
+  <!-- Pulls in footer.html -->
+<div data-include="components/footer.html"></div>
+
+  <!-- script-->
+  <script src="scripts/HTMLInclude.js"></script>
+  
+    <footer>
+        <!-- Validator script (optional for validation cloud) -->
+        <script src="https://lint.page/kit/880bd5.js" crossorigin="anonymous"></script>
+    </footer>
+
+</body>
+
+</html>
