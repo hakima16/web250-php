b2f98c8bbcd7cb9968a8e4ecab04feb29faab63f
hakima16 <chhakima87@gmail.com>
2026-03-09 17:09:04 -0400

    Create footer.php

---
+++ components/footer.php
@@ -0,0 +1,23 @@
+<footer>
+    <a href="https://github.com/hakima16/" target="_blank">GitHub</a> |
+    <a href="https://hakima16.github.io/" target="_blank">GitHub.io</a> |
+    <a href="https://hakima16.github.io/web115/" target="_blank">WEB115</a> |
+	<a href="https://hakima16.github.io/web250" target="_blank">WEB250</a> |
+	<a href="https://hakima16.github.io/web215" target="_blank">WEB215</a> |
+    <a href="https://www.freecodecamp.org/kima16" target="_blank">freeCodeCamp</a> |
+    <a href="https://www.codecademy.com/profiles/kima16" target="_blank">Codecademy</a> |
+    <a href="https://www.linkedin.com/in/hakima-chabane-08674a1a4/" target="_blank">LinkedIn</a>
+
+    <!-- your links and attribution -->
+    <p>Page built by Hakima’s Helpful Crane</p>
+  
+   
+   <!-- Validator script -->
+   <script src="https://lint.page/kit/880bd5.js" crossorigin="anonymous"></script>
+
+
+    
+  </footer>
+  
+
+
