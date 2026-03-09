13ae9b1ee802cb10cfbdd34ec6fce2a38576b370
hakima16 <chhakima87@gmail.com>
2026-03-09 17:09:16 -0400

    Create contract.php

---
+++ contract.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Metadata -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contract - Hakima Chabane WEB250</title>
+  <!-- Link to CSS file -->
+  <link rel="stylesheet" type="text/css" href="styles/default.css">
+</head>
+
+
+<body>
+<!-- Pulls in header.html -->
+<div data-include="components/header.html"></div>
+
+  <main>
+    <h2>Contract</h2>
+
+    <p>
+      I,<a href="introduction.html">Hakima Chabane</a>, agree to abide by the terms of
+      the course contract in my WEB250 (spring 2026) with my instructor,
+      D.I. von Briesen.
+    </p>
+
+    <p>
+      I understand that all work that I do on my school and personal website will
+      be publicly available to the world, and will not put information there that is
+      inappropriate for schoolwork, or that I wish to keep private.
+    </p>
+
+    <p>
+      I also understand that it is my work that counts for attendance, not logins
+      or showing up for class. As such, failure to turn in assignments may show as absences.
+    </p>
+
+    <p>Signed: <em>Hakima Chabane, 01/23/2026</em></p>
+  </main>
+
+  <!-- Pulls in footer.html -->
+<div data-include="components/footer.html"></div>
+
+  <!-- script-->
+  <script src="scripts/HTMLInclude.js"></script>
+  
+  <footer>
+    <!-- Validator script -->
+    <script src="https://lint.page/kit/880bd5.js" crossorigin="anonymous"></script>
+  </footer>
+
+</body>
+
+</html>
