53233e8d4165cd9f11d52d0689e01d613bc46c2a
hakima16 <chhakima87@gmail.com>
2026-03-15 19:18:02 -0400

    Update header.php

--- components/header.php
+++ components/header.php
@@ -1,9 +1,10 @@
 <header>
-    <!-- Page Header -->
-    <h1>Hakima Chabane's Helpful Crane | WEB250</h1>
-    <nav>
-      <a href="index.html">Home</a> |
-      <a href="introduction.html">Introduction</a> |
-      <a href="contract.html">Contract</a>
-	  </nav>
-  </header>
+  <h1>Hakima Chabane's Helpful Crane | WEB250 PHP Site</h1>
+  <nav>
+    <a href="../index.html">Course Home</a> |
+    <a href="../superduper_static/index.html">Multipage Static</a> |
+    <a href="index.php?page=home">Home</a> |
+    <a href="index.php?page=about">About</a> |
+    <a href="index.php?page=contact">Contact</a>
+  </nav>
+</header>
