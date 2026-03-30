ae3d0cf3620c0f6e6f09f54d75d2bc3dffa794c0
hakima16 <chhakima87@gmail.com>
2026-03-30 12:48:49 -0400

    Create contract.php

---
+++ contents/contract.php
@@ -0,0 +1,20 @@
+<h2>Contract</h2>
+
+    <p>
+      I,<a href="introduction.html">Hakima Chabane</a>, agree to abide by the terms of
+      the course contract in my WEB250 (spring 2026) with my instructor,
+      D.I. von Briesen.
+    </p>
+
+    <p>
+      I understand that all work that I do on my school and personal website will
+      be publicly available to the world, and will not put information there that is
+      inappropriate for schoolwork, or that I wish to keep private.
+    </p>
+
+    <p>
+      I also understand that it is my work that counts for attendance, not logins
+      or showing up for class. As such, failure to turn in assignments may show as absences.
+    </p>
+
+    <p>Signed: <em>Hakima Chabane, 01/23/2026</em></p>
